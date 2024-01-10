f3cd123777d4210ea7eadd1886297e5295326ccc
oriazadok <oriazadok@gmail.com>
2024-01-10 12:11:18 +0200

    add filterdata comonent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import RecruiterProfile from './pages/RecruiterProfile';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        const data = await response.text();
+        setMessage(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        <Route path="/signin" element={<VolunterSearchProfile />} />
-        <Route path="/profile" element={<RecruiterProfile />} />
-=======
->>>>>>> Stashed changes
+        <Route path="/" element={<Home message={message} />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/recruiter_profile" element={<RecruiterProfile />} />
 >>>>>>> origin/master
