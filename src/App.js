fa0cbc4e87c7fa3b7db11aff5ee1aae1cea1c541
sagir567 <sagir567@gmail.com>
2024-01-10 14:47:12 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -8,16 +8,13 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+
+
         <Route path="/signin" element={<VolunterSearchProfile />} />
         <Route path="/profile" element={<RecruiterProfile />} />
-=======
->>>>>>> Stashed changes
+
         <Route path="/signin" element={<SignIn />} />
         <Route path="/recruiter_profile" element={<RecruiterProfile />} />
->>>>>>> origin/master
       </Routes>
     </Router>
   );
