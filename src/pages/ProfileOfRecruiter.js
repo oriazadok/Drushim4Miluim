2309eaaa20081b18444485038ec4071f0ec905ea
noyrosenbaum <noyrosen123@gmail.com>
2024-01-15 23:17:06 +0200

    Format html

--- src/pages/ProfileOfRecruiter.js
+++ src/pages/ProfileOfRecruiter.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const ProfileOfRecruiter = () => {
+
+const ProfileOfRecruiter = ({ name, gender, age, phoneNumber, Email, unit }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <h2>Recruiter Profile</h2>
+      <p>Name: {name}</p>
+      <p>Gender: {gender}</p>
+      <p>Age: {age}</p>
+      <p>Phone Number: {phoneNumber}</p>
+      <p>Email: {Email}</p>
+      <p>Email: {unit}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
 
 export default ProfileOfRecruiter;
--- src/pages/ProfileOfVolunteer.js
+++ src/pages/ProfileOfVolunteer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const ProfileOfVolunteer = () => {
+const ProfileOfVolunteer = ({ name, gender, age, phoneNumber, Email, skills, rovai, sadir, ptor}) => {
   return (
-    <div>Profile volunteer</div>
-  )
-}
+    <div>
+      <h2>Volunteer Profile</h2>
+      <p>Name: {name}</p>
+      <p>Gender: {gender}</p>
+      <p>Age: {age}</p>
+      <p>Phone Number: {phoneNumber}</p>
+      <p>Email: {Email}</p>
+      <p>Skills: {skills}</p>
+      <p>Rovai: {rovai}</p>
+      <p>Sadir: {sadir}</p>
+      <p>Ptor: {ptor}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
 
 export default ProfileOfVolunteer;
