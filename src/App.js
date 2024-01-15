0f9259b83c6b5be431faf701eee7d885bf728027
sagir567 <sagir567@gmail.com>
2024-01-15 14:02:15 +0200

    delete recruiter profile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Recruiter from './pages/Recruiter';
+import VolunterSearchProfile from './pages/VolunterSearchProfile';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<VolunterSearchProfile />} />
-        <Route path="/profile" element={<RecruiterProfile />} />
+        <Route path="/profile" element={<Recruiter />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/recruiter" element={<Recruiter />} />
