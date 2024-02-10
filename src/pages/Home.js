dc574f841c40f8a5020555fcc53b5f22da916836
aronl096 <aronl411@gmail.com>
2024-02-10 20:25:44 +0200

    new changes on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import '../style//Home.css'; // Import the CSS file to add styles
+import { Link } from 'react-router-dom';
+import tank from '../images/tank.jpg'
 
-// import Header from '../components/Header';
-import Navigator from '../components/Navigator';
-
-const Home = () => {
+const Home = ({userType}) => {
   return (
-    <div>
-        {/* <Header /> */}
-        <Navigator />
-        <h1>Drushim4Miluim</h1>
-        
-        
+    <div className="container"> {/* Apply a container for the elements of the page */}
+      <h1 className="title">ברוכים הבאים לדרושים במילואים</h1>
+      <p className="description">במהלך מלחמת "לרבות ברזל" הוחלט על הקמת פרויקט חדש על מנת להקל
+      על הליכי הגיוס למילואים ומעבר בין יחידות שך מילואים שונות
+      הניכם מוזמנים להצטרף לקהילת "דרושים למילואים" הגדולה בארץ ובכך לתרום 
+      לצבא החזק , מוסרי והאיכותי ביותר בעולם!!!</p>
+      <img src={tank} alt="Descripción de la imagen" className="image" /> {/* Add an image */}
+      <Link to={userType === 'recruiter' ? "/recruiterHome" : userType === 'volunteer' ? "/volunteerHome" : "/signin"}>
+        <button>לאיזור האישי</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
