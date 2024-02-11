ddbb7e4006c39dce76741f2a6a43b400808deda9
taltosiano <94299489+taltosiano@users.noreply.github.com>
2024-02-11 15:41:28 +0200

    Update VolunteerSearch.js

--- src/pages/VolunteerSearch.js
+++ src/pages/VolunteerSearch.js
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 
 import Navigator from '../components/Navigator';
 
-import AddPosition from '../components/AddPosition';
-// import Filter from '../components/Filter';
+import PositionFilter from '../components/PositionsFilter';
+import Filter from '../components/Filter';
 import FilterData from '../components/FilterData';
 import Volunteers from '../components/Volunteers';
+import Position from '../components/Position';
+import Positions from './Positions';
 
-const RecruiterSearch = () => {
+const VolunteerSearch = () => {
   const [showAddPosition, setShowAddPosition] = useState(false);   // Manage AddPosition visibility
   const [showFilter, setShowFilter] = useState(false);             // Manage Filter visibility
   const [filterData, setFilterData] = useState({                   // Manage FilterData's data
-    מיקום: '',
-    שירות: '',
-    שחרור: '',
-    רובאי: '',
-    פרופיל: '',
-    מגיל: '',
-    עד: '',
+    location: '',
+    service: '',
+    rovai: '',
+    profile: '',
+    fromAge: '',
+    untilAge: '',
   });
 
   // This fuction handle the visibility of the button and the AddPosition component
@@ -56,23 +57,9 @@ const RecruiterSearch = () => {
   const shouldShowFilterData = Object.values(filterData).some((value) => value !== '');
 
   return (
-    <div className="recruiter-profile-container">
+    <div className="volunteers-profile-container">
       <Navigator />
       <h1 className="profile-heading">שלום מתנדב יקר</h1>
-      <div className="button-container">
-        {/* Visibility of "Add Position" button */}
-        {!showAddPosition && (
-          <button className="toggle-button" onClick={addPosition}>הוסף משרה</button>
-        )}
-        
-        {/* Visibility of "Add Position" */}
-        {showAddPosition && (
-          <AddPosition
-            onPositionAdded={handlePositionAdded}
-            onCancel={handleCancelAddPosition}
-          />
-        )}
-      </div>
 
       <div className="button-container">
         {/* Visibility of "Filter" button */}
@@ -81,14 +68,14 @@ const RecruiterSearch = () => {
         )}
 
         {/* Visibility of "Filter" */}
-        {/* {showFilter && (
+        {showFilter && (
             <Filter
               onFilterChange={handleFilterChange}
               handleFilter={filter}
               onCancel={cancelFilter}
               initialFilters={filterData}
             />
-        )} */}
+        )}
       </div>
       
       {/* Visibility of "FilterData" */}
@@ -96,9 +83,9 @@ const RecruiterSearch = () => {
         <FilterData data={filterData} />
       )}
 
-      <Volunteers />
+      <Position />
     </div>
   );
 };
 
-export default RecruiterSearch;
+export default VolunteerSearch;
