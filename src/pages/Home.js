3ba6399dc0e59d855ba18fe30c7f826db39517c7
aronl096 <aronl411@gmail.com>
2024-02-11 13:24:00 +0200

    New version of Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import '../style//Home.css'; // Import the CSS file to add styles
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import tank from '../images/tank.jpg'
 
-const Home = ({userType}) => {
+import tank from '../images/tank.jpg'
+import '../style//Home.css'; // Import the CSS file to add styles
+
+const Home = () => {
+ 
+  const [userData, setUserData] = useState({}); // Define state for user data using the useState hook
+
+  useEffect(() => {
+    // Retrieve user data from localStorage
+    const storedUserData = localStorage.getItem('userData');
+
+    if (storedUserData !== null) {
+      const userData = JSON.parse(storedUserData);
+
+      if(userData !== null) {
+        setUserData(userData);
+
+      }
+    }
+  }, []); 
   return (
     <div className="container"> {/* Apply a container for the elements of the page */}
       <h1 className="title">ברוכים הבאים לדרושים במילואים</h1>
@@ -12,7 +29,7 @@ const Home = ({userType}) => {
       הניכם מוזמנים להצטרף לקהילת "דרושים למילואים" הגדולה בארץ ובכך לתרום 
       לצבא החזק , מוסרי והאיכותי ביותר בעולם!!!</p>
       <img src={tank} alt="Descripción de la imagen" className="image" /> {/* Add an image */}
-      <Link to={userType === 'recruiter' ? "/recruiterHome" : userType === 'volunteer' ? "/volunteerHome" : "/signin"}>
+      <Link to={userData && userData.type === "recruiters" ? "/recruiterHome" : userData.type === "volunteers" ? "/volunteerHome" : "/signin"}> 
         <button>לאיזור האישי</button>
       </Link>
     </div>
