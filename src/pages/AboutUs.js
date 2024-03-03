22f3e4de65a23578d994cdfb20041dcb25924ba0
aronl096 <aronl411@gmail.com>
2024-03-03 05:41:00 +0200

    new homepage design

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,21 @@ import '../style//AboutUs.css'; // Import the CSS file to add styles
 import { Link } from 'react-router-dom';
 import React from 'react';
 import tzav8 from '../images/tzav8.jpg'
+import aaron from '../images/aaron.jpg'
+import sagi from '../images/sagi.jpg'
+import elad from '../images/elad.jpg'
+import tzadok from '../images/tzadok.jpg'
 
 const AboutUs = () => {
     return (
         <div className="AboutUs">
             <header className="AboutUsHeader">
+                <img src={aaron} alt="Aaron" className="image aaron" />
+                <img src={sagi} alt="Sagi" className="image sagi" />
+                <img src={elad} alt="Elad" className="image elad" />
+                <img src={tzadok} alt="Tzadok" className="image tzadok" />
                 <img src={tzav8} alt="Descripción de la imagen" className="image" /> {/* Add an image */}
+                
                 <p className="description">
                 This project was developed in order to help recruiters and volunteers for miluim find each other.
                 If you want to volunteer, you can enter your details and start looking for your best match position.
